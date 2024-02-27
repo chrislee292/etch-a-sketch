5d43900b5250a2b2ca5bbb6ffeaf92170a44cb5f
Christopher Lee <98063243+chrislee292@users.noreply.github.com>
2024-02-27 17:23:49 -0600

    Create mouseover effect for each div in grid

--- javascript.js
+++ javascript.js
@@ -1,14 +1,29 @@
+// Create grid when webpage opens
+
 function createGrid() {
     const container = document.querySelector('#grid-container');
-    const gridSize = 16;
+    const gridLen = 4;
     
     container.innerHTML = '';
 
-    for (let i = 0; i < gridSize; i++) {
+    for (let i = 0; i < gridLen ** 2; i++) {
         const gridItem = document.createElement('div');
         gridItem.classList.add('grid-item');
         container.appendChild(gridItem);
     }
 }
 
+function hoverEffect() {
+    // Select .grid-item elements within #grid-container
+    const gridItems = document.querySelectorAll('#grid-container .grid-item');
+
+    // Attach a 'mouseover' event listener to each .grid-item element
+    gridItems.forEach((gridItem) => {
+        gridItem.addEventListener('mouseover', () => {
+            gridItem.style.backgroundColor = 'black';
+        });
+    });
+}
+
 createGrid();
+hoverEffect();
