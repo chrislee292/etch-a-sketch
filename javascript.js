f437c4ca9c12d00698a726183fb55977847d77d1
Christopher Lee <98063243+chrislee292@users.noreply.github.com>
2024-02-27 18:06:39 -0600

    Create reset grid and slider

--- javascript.js
+++ javascript.js
@@ -1,11 +1,24 @@
 // Create grid when webpage opens
 
+const slider = document.querySelector('#slider');
+let gridLen = slider.value;
+
+slider.addEventListener('input', () => {
+    gridLen = slider.value;
+    createGrid();
+    hoverEffect();
+    resetGrid();
+});
+
 function createGrid() {
+
+    // Select grid container
     const container = document.querySelector('#grid-container');
-    const gridLen = 4;
     
+    // Clear contents of container
     container.innerHTML = '';
 
+    // Create gridLen squared grid items
     for (let i = 0; i < gridLen ** 2; i++) {
         const gridItem = document.createElement('div');
         gridItem.classList.add('grid-item');
@@ -25,5 +38,17 @@ function hoverEffect() {
     });
 }
 
+function resetGrid() {
+    const resetButton = document.querySelector('#reset');
+    resetButton.addEventListener('click', () => {
+        const gridItems = document.querySelectorAll('#grid-container .grid-item');
+    
+        gridItems.forEach((gridItem) => {
+            gridItem.style.backgroundColor = 'white';
+        });
+    });
+}
+
 createGrid();
 hoverEffect();
+resetGrid();
